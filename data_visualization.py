91b4f0bc5d6e51a0ad5602bf9f1e464ac22b340f
leTabs <125308756+leTabs@users.noreply.github.com>
2023-03-19 22:10:56 +0200

    Update data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as mpl
+from matplotlib.ticker import FixedLocator, FixedFormatter
 import numpy as np
 
 countries = np.array([
@@ -18,10 +19,25 @@ countries = np.array([
 'Israel',
 'Greece'
 ])
-percentage = np.array([25,26,30,30,33,35,37,39,40,42,45,46,47,48,62])
+lonliness_per = np.array([25,26,30,30,33,35,37,39,40,42,45,46,47,48,62])
+rfriend_per =np.array([95.3,94, 91.8, 89.9, 93.2,90.1, 92, 94.6, 94.8, 91.7, 88.5,
+91.5, 90.8, 86.6, 82.3])
+rfriend_per = sorted(rfriend_per)
 
-fig, ax  = mpl.subplots()
-ax.bar(countries, percentage)
-ax.set_xticklabels(countries, rotation=-90)
+mpl.subplot(2,2,1)
+mpl.grid()
+mpl.plot(lonliness_per, countries)
+
+mpl.subplot(2,2,2)
+mpl.grid()
+mpl.bar(lonliness_per, countries)
+
+mpl.subplot(2,2,3)
+mpl.plot(rfriend_per, countries)
+mpl.grid()
+
+mpl.subplot(2,2,4)
+mpl.bar(rfriend_per, countries)
+mpl.grid()
 
 mpl.show()
