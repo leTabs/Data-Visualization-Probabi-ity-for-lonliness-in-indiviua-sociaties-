6c951a3ba0edcee20675527fd49f4dcfd6731c51
leTabs <125308756+leTabs@users.noreply.github.com>
2023-03-19 21:27:32 +0200

    Add files via upload

---
+++ data_visualization.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as mpl
+import numpy as np
+
+x = np.array([
+'Denmark',
+'Switzerland', 
+'Sweden',
+'US',
+'England',
+'Netherlandas',
+'Germany',
+'Finland',
+'Spain',
+'Belgium',
+'France',
+'Austria',
+'Italy',
+'Israel',
+'Greece'
+])
+y = np.array([25,26,30,30,33,35,37,39,40,42,45,46,47,48,62])
