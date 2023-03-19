71b94238138f9707ec1869d98d20842718523be8
leTabs <125308756+leTabs@users.noreply.github.com>
2023-03-19 22:52:25 +0200

    Update data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -24,20 +24,40 @@ rfriend_per =np.array([95.3,94, 91.8, 89.9, 93.2,90.1, 92, 94.6, 94.8, 91.7, 88.
 91.5, 90.8, 86.6, 82.3])
 rfriend_per = sorted(rfriend_per)
 
+font_styles = {
+  'family' :'serif', 
+  'color': 'purple', 
+  'weight' : 'normal', 
+  'size': 15
+}
+
+
+mpl.suptitle('Lonliness & Friends trust per ')
 mpl.subplot(2,2,1)
 mpl.grid()
-mpl.plot(lonliness_per, countries)
-
-mpl.subplot(2,2,2)
-mpl.grid()
-mpl.bar(lonliness_per, countries)
+mpl.title('Lonliness per Country plot')
+mpl.ylabel('Countries')
+mpl.xlabel('Lonliness in %')
+mpl.plot(lonliness_per, countries, 'o-', color = "#15E79E")
 
 mpl.subplot(2,2,3)
-mpl.plot(rfriend_per, countries)
+mpl.grid(axis = "y", color = "gray")
+mpl.ylabel('Countries')
+mpl.xlabel('Lonliness in %')
+mpl.bar(lonliness_per, countries, color = "#15E79E")
+
+mpl.subplot(2,2,2)
+mpl.title('Friend Trust per Country')
 mpl.grid()
+mpl.ylabel('Countries')
+mpl.xlabel('Friend trust in %')
+mpl.plot(rfriend_per, countries, 'o-', color = "#6BCA00")
+
 
 mpl.subplot(2,2,4)
-mpl.bar(rfriend_per, countries)
-mpl.grid()
+mpl.grid(axis = "y", color = "gray")
+mpl.ylabel('Countries')
+mpl.xlabel('Friend trust in %')
+mpl.bar(rfriend_per, countries, color = "#6BCA00")
 
 mpl.show()
